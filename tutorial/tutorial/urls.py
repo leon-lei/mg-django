eaf40b83846cd9f41426a0f34544973fefbfb43d
Leon Lei <leonlei35t@gmail.com>
2018-04-09 23:44:59 -0400

    Navbar and user profile model

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from tutorial import views
 
 urlpatterns = [
+    path('', views.login_redirect, name='login_redirect'),
     path('admin/', admin.site.urls),
     path('account/', include('accounts.urls')),
 ]
