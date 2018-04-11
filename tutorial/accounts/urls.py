8433b92a3a9f052f0f692c3734afa85cab368474
Leon Lei <leonlei35t@gmail.com>
2018-04-10 23:30:28 -0400

    Created registration and profile htmls

--- tutorial/accounts/urls.py
+++ tutorial/accounts/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home),
     path('login/', login, {'template_name':'accounts/login.html'}),
-    path('logout/', logout, {'template_name':'accounts/logout.html'})
+    path('logout/', logout, {'template_name':'accounts/logout.html'}),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile')
 ]
--- tutorial/accounts/views.py
+++ tutorial/accounts/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render, HttpResponse
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from accounts.forms import RegistrationForm
 
 def home(request):
-	numbers = [1,2,3,4,5]
-	name = 'Leon Lei'
-	args = {'myName':name, 'numbers':numbers}
-	return render(request, 'accounts/home.html', args)
+    numbers = [1,2,3,4,5]
+    name = 'Leon Lei'
+    args = {'myName':name, 'numbers':numbers}
+    return render(request, 'accounts/home.html', args)
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/account')
+    else:
+        form = RegistrationForm()
+
+        args = {'form':form}
+        return render(request, 'accounts/reg_form.html',args)
+
+def profile(request):
+    args = {'user': request.user}
+    return render(request, 'accounts/profile.html', args)
