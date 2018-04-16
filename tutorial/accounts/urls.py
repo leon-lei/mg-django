b3124a80011edd13e1cf5174281077a2f2144482
Leon Lei <leonlei35t@gmail.com>
2018-04-15 20:25:58 -0400

    Added password reset

--- tutorial/accounts/urls.py
+++ tutorial/accounts/urls.py
@@ -1,5 +1,8 @@
 from django.urls import include, path
-from django.contrib.auth.views import login, logout
+from django.contrib.auth.views import (
+	login, logout, password_reset, password_reset_done, password_reset_confirm,
+	password_reset_complete
+)
 from . import views
 
 urlpatterns = [
@@ -7,5 +10,11 @@ urlpatterns = [
     path('login/', login, {'template_name':'accounts/login.html'}),
     path('logout/', logout, {'template_name':'accounts/logout.html'}),
     path('register/', views.register, name='register'),
-    path('profile/', views.profile, name='profile')
+    path('profile/', views.view_profile, name='view_profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('change-password/', views.change_password, name='change_password'),
+    path('reset-password/', password_reset, name='reset_password'),
+    path('reset-password/done/', password_reset_done, name='password_reset_done'),
+    path('reset-password/confirm/<uidb64>/<token>/', password_reset_confirm, name='password_reset_confirm'),
+    path('reset-password/complete/', password_reset_complete, name='password_reset_complete'),
 ]
--- tutorial/accounts/views.py
+++ tutorial/accounts/views.py
@@ -1,7 +1,15 @@
-from django.contrib.auth.models import User
-from django.shortcuts import render, redirect
-from accounts.forms import RegistrationForm
 
+from django.shortcuts import render, redirect
+from accounts.forms import (
+    RegistrationForm, 
+    EditProfileForm, 
+)
+from django.contrib.auth import update_session_auth_hash
+from django.contrib.auth.forms import UserChangeForm, PasswordChangeForm
+from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
+
+@login_required
 def home(request):
     numbers = [1,2,3,4,5]
     name = 'Leon Lei'
@@ -20,6 +28,36 @@ def register(request):
         args = {'form':form}
         return render(request, 'accounts/reg_form.html',args)
 
-def profile(request):
+@login_required
+def view_profile(request):
     args = {'user': request.user}
     return render(request, 'accounts/profile.html', args)
+
+@login_required
+def edit_profile(request):
+    if request.method == 'POST':
+        form = EditProfileForm(request.POST, instance=request.user)
+
+        if form.is_valid():
+            form.save()
+            return redirect('/account/profile')
+    else:
+        form = EditProfileForm(instance=request.user)
+        args = {'form': form}
+        return render(request, 'accounts/edit_profile.html', args)
+
+@login_required
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(data=request.POST, user=request.user)
+
+        if form.is_valid():
+            form.save()
+            update_session_auth_hash(request, form.user)
+            return redirect('/account/profile')
+        else:
+            return redirect('/account/change-password')
+    else:
+        form = PasswordChangeForm(user=request.user)
+        args = {'form': form}
+        return render(request, 'accounts/change_password.html', args)
