143cde12d45289b633dc10f3835cdb981ca72510
Leon Lei <leonlei35t@gmail.com>
2018-04-22 17:09:01 -0400

    Created MEDIA_URL and MEDIA_ROOT

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from tutorial import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.login_redirect, name='login_redirect'),
     path('admin/', admin.site.urls),
     path('account/', include('accounts.urls', namespace='accounts')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
