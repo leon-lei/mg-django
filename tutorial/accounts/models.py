2c53b4a5699d9f81ffe8607cd535c7f16e9973ed
Leon Lei <leonlei35t@gmail.com>
2018-04-22 20:11:15 -0400

    Created UserProfileManager in models

--- tutorial/accounts/models.py
+++ tutorial/accounts/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 
+class UserProfileManager(models.Manager):
+    def get_queryset(self):
+        return super(UserProfileManager, self).get_queryset().filter(city='London')
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     description = models.CharField(max_length=100, default='')
@@ -9,6 +13,8 @@ class UserProfile(models.Model):
     website = models.URLField(default='')
     phone = models.IntegerField(default=0)
     image = models.ImageField(upload_to='profile_image', blank=True)
+
+    london = UserProfileManager( )
 
     def __str__(self):
         return self.user.username
