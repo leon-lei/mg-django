ce9394c3b14f61e34f6ba298d2232c0161d24c66
Leon Lei <leonlei35t@gmail.com>
2018-04-26 08:20:56 -0400

    Added Friend model and pk for users

--- tutorial/accounts/urls.py
+++ tutorial/accounts/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('logout/', logout, {'template_name':'accounts/logout.html'}, name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.view_profile, name='view_profile'),
+    path('profile/<pk>/', views.view_profile, name='view_profile_with_pk'),
     path('profile/edit/', views.edit_profile, name='edit_profile'),
     path('change-password/', views.change_password, name='change_password'),
 
