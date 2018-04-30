1c8d4656939d37195cd507c1d56b9e50da7e7330
Leon Lei <leonlei35t@gmail.com>
2018-04-29 23:48:00 -0400

    Added make and lose friend methods to Friend model

--- tutorial/home/views.py
+++ tutorial/home/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 
 from home.forms import HomeForm
-from home.models import Post
+from home.models import Post, Friend
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
@@ -29,3 +29,11 @@ class HomeView(TemplateView):
 
         args = {'form': form, 'text':text}
         return render(request, self.template_name, args)
+
+def change_friends(request, operation, pk):
+    new_friend = User.objects.get(pk=pk)
+    if operation == 'add':
+        Friend.make_friend(request.user, new_friend)
+    elif operation == 'remove':
+        Friend.lose_friend(request.user, new_friend)
+    return redirect('home:home')
