3737994cb6272a4cb33cb6c803a8ed940791447f
Leon Lei <leonlei35t@gmail.com>
2018-05-17 00:59:15 -0400

    Registered EventAdmin and TribeAdmin

--- tutorial/tribes/admin.py
+++ tutorial/tribes/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
-from .models import Event, Tribe
+from tribes.models import Event, Tribe
 
-admin.site.register(Event)
-admin.site.register(Tribe)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('event_date', 'event_name', 'location', 'tribe')
+
+    def get_queryset(self, request):
+        queryset = super(EventAdmin, self).get_queryset(request)
+        queryset = queryset.order_by('-event_date', 'event_name')
+        return queryset
+
+class TribeAdmin(admin.ModelAdmin):
+    list_display = ('tribe_name', 'chieftain', 'created')
+
+    def get_queryset(self, request):
+        queryset = super(TribeAdmin, self).get_queryset(request)
+        queryset = queryset.order_by('-tribe_name', 'chieftain')
+        return queryset
+
+admin.site.register(Event, EventAdmin)
+admin.site.register(Tribe, TribeAdmin)
