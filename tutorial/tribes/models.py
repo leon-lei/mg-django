89698fb99662e77f9fcafdf513e850d01e199ae6
Leon Lei <leonlei35t@gmail.com>
2018-05-22 00:04:56 -0400

    Deleted db.sqlite3. Changed fields of Event and Tribe models

--- tutorial/tribes/models.py
+++ tutorial/tribes/models.py
@@ -1,24 +1,9 @@
 from django.db import models
 from accounts.models import UserProfile
 
-class Event(models.Model):
-    event_name = models.CharField(max_length=250, blank=True)
-    event_date = models.DateField(blank=True)
-    location = models.CharField(max_length=250, blank=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    image = models.ImageField(upload_to='event_image', blank=True)
-
-    def __str__(self):
-        return self.event_name
-
-    class Meta:
-        ordering = ('event_date',)
-
 class Tribe(models.Model):
     tribe_name = models.CharField(max_length=250)
     chieftain = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-    events = models.ManyToManyField(Event, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='tribe_image', blank=True)
@@ -28,3 +13,18 @@ class Tribe(models.Model):
 
     class Meta:
         ordering = ('tribe_name',)
+
+class Event(models.Model):
+    event_name = models.CharField(max_length=250, blank=True)
+    event_date = models.DateField(blank=True)
+    location = models.CharField(max_length=250, blank=True)
+    tribe = models.ForeignKey(Tribe, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    image = models.ImageField(upload_to='event_image', blank=True)
+
+    def __str__(self):
+        return self.event_name
+
+    class Meta:
+        ordering = ('event_date',)
